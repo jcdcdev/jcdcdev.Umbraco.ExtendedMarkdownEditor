8c09a60a9a2870dcb206f9d908da8c78c6823a0e
jcdcdev <james@jcdc.dev>
2024-10-22 21:10:43 +0100

    fix: front end build issue

---
+++ src/jcdcdev.Umbraco.ExtendedMarkdownEditor/Composing/PackageManifestReader.cs
@@ -0,0 +1,32 @@
+﻿using jcdcdev.Umbraco.Core.Extensions;
+using jcdcdev.Umbraco.Core.Web.Models.Manifests;
+using Umbraco.Cms.Core.Manifest;
+using Umbraco.Cms.Infrastructure.Manifest;
+
+namespace jcdcdev.Umbraco.ExtendedMarkdownEditor.Composing;
+
+public class PackageManifestReader : IPackageManifestReader
+{
+    public Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
+    {
+        var extensions = new List<IManifest>();
+        var packageManifest = new PackageManifest
+        {
+            Name = Constants.PackageName,
+            Version = EnvironmentExtensions.CurrentAssemblyVersion().ToSemVer()?.ToString() ?? "0.1.0",
+            AllowPublicAccess = false,
+            AllowTelemetry = true,
+            Extensions = []
+        };
+
+        extensions.Add(new BackofficeEntryPointManifest()
+        {
+            Name = "extended-markdown-editor.entrypoint",
+            Alias = "extended-markdown-editor.entrypoint",
+            Js = "/App_Plugins/jcdcdev.Umbraco.ExtendedMarkdownEditor/dist/extended-markdown-editor.js"
+        });
+
+        packageManifest.Extensions = extensions.OfType<object>().ToArray();
+        return Task.FromResult<IEnumerable<PackageManifest>>([packageManifest]);
+    }
+}
---
+++ src/jcdcdev.Umbraco.ExtendedMarkdownEditor/Constants.cs
@@ -0,0 +1,6 @@
+namespace jcdcdev.Umbraco.ExtendedMarkdownEditor;
+
+public static class Constants
+{
+    public const string PackageName = "Extended Markdown Editor";
+}
